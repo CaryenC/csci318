f6054aac2a0eb3f59e0ed0fd2371e787c1c918b5
CaryenC <work.caryenc@gmail.com>
2021-09-25 13:48:00 +0800

    FEATS: Scroll up button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Routes from './routes'
+import {CircleArrow} from "react-scroll-up-button";
 
 function App() {
   return (
     <div className="App">
+        <CircleArrow 
+          EasingType="linear"
+          ShowAtPosition={100}
+          AnimationDuration={100} />
         <Routes/>
     </div>
   );
